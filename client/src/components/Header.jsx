101ce77d06cbf0f687bf8b0c9e6aa5aae28a4e38
Anirudhchauhan04 <chauhan.anirudh004@gmail.com>
2024-07-02 23:41:02 +0530

    update header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,11 +1,13 @@
-import { Button, Navbar, TextInput } from "flowbite-react";
+import { Avatar, Button, Dropdown, Navbar, TextInput } from "flowbite-react";
 import { Link , useLocation} from "react-router-dom";
 import {AiOutlineSearch} from "react-icons/ai";
 import {FaMoon} from "react-icons/fa";
+import {useSelector} from 'react-redux';
 
 
 export default function Header() {
   const path = useLocation().pathname;
+  const {currentUser} = useSelector(state => state.user)
   return (
     <Navbar className=" border bottom-2">
         <Link
@@ -33,11 +35,31 @@ export default function Header() {
           pill>
           <FaMoon/>
         </Button>
-        <Link to='/sign-in'>
+        {
+          currentUser ? (
+           <Dropdown arrowIcon={false} inline label= {<Avatar alt="user"
+            img={currentUser.profilePicture}
+            rounded
+           />}>
+            <Dropdown.Header>
+              <span className=" block text-sm">@{currentUser.username}</span>
+              <span className=" block text-sm font-medium truncate">{currentUser.email}</span>
+            </Dropdown.Header>
+            <Link to = {'/dashboard?tab=profile'}>
+            <Dropdown.Item>Profile</Dropdown.Item>
+            </Link>
+            <Dropdown.Divider/>
+            <Dropdown.Item>Sign out</Dropdown.Item>
+           </Dropdown> 
+          ):
+          (
+            <Link to='/sign-in'>
             <Button gradientDuoTone='purpleToBlue' outline>
               Sign In
             </Button>
           </Link>
+          )
+        }
           <Navbar.Toggle/>
       </div>
       <Navbar.Collapse>
