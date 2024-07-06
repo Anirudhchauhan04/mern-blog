411401d873b5e43e255e3182c4bc70b0e5c79662
Anirudhchauhan04 <chauhan.anirudh004@gmail.com>
2024-07-06 23:54:10 +0530

    show to users to admin dashbord

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -3,6 +3,7 @@ import {
   HiUser,
   HiArrowSmRight,
   HiDocumentText,
+  HiOutlineUserGroup,
 } from 'react-icons/hi';
 import { useEffect, useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
@@ -61,6 +62,17 @@ export default function DashSidebar() {
               as='div'
             >
               Posts
+            </Sidebar.Item>
+            </Link>
+          ) }
+          {currentUser.isAdmin && (
+            <Link to='/dashboard?tab=users'>
+            <Sidebar.Item
+              active={tab === 'users'}
+              icon={HiOutlineUserGroup}
+              as='div'
+            >
+              Users
             </Sidebar.Item>
             </Link>
           ) }
