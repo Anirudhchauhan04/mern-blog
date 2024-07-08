9d0ca000cacf5242ce0d98fae1430a1f9a104f8f
Anirudhchauhan04 <chauhan.anirudh004@gmail.com>
2024-07-08 17:12:06 +0530

    finish

--- api/index.js
+++ api/index.js
@@ -34,7 +34,8 @@ app.use('/api/comment', commentRoutes);
 app.use(express.static(path.join(__dirname,'/client/dist' )));
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname,'client','dist','index.html'));
+    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+  });
 
 
 
